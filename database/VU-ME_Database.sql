7e4957beec8b225b736a65aa6de7285f41440de5
Nicholas Rader <201325732@student.uj.ac.za>
2017-02-22 20:10:12 +0200

    *Database SQL file written

---
+++ database/VU-ME_Database.sql
@@ -0,0 +1,82 @@
+DROP DATABASE IF EXISTS `ViewMe`;
+CREATE DATABASE `ViewMe`;
+USE `ViewMe`;
+
+DROP TABLE IF EXISTS 'REGISTERED_USER' ;
+CREATE TABLE 'REGISTERED_USER'(
+  `UserID` integer not null auto_increment,
+  `Username` varchar(40) not null UNIQUE,
+  `Name` varchar(40) not null,
+  `Surname` varchar(40) not null,
+  `Email` varchar(40) not null UNIQUE ,
+  `Password` varchar(255) not null,
+  `UserType` tinyint not null,
+  PRIMARY KEY (`UserID`)
+);
+
+DROP TABLE IF EXISTS 'FOLLOWERS' ;
+CREATE TABLE 'FOLLOWERS'(
+  `UserID` integer not null,
+  `FollowerID` integer not null,
+  FOREIGN KEY (`UserID`) REFERENCES REGISTERED_USER(`UserID`)
+
+);
+
+DROP TABLE IF EXISTS 'FOLLOWING' ;
+CREATE TABLE 'FOLLOWING'(
+  `UserID` integer not null,
+  `FollowingID` integer not null,
+  FOREIGN KEY (`UserID`) REFERENCES REGISTERED_USER(`UserID`)
+);
+
+DROP TABLE IF EXISTS 'POST' ;
+CREATE TABLE 'POST'(
+  `PostID` integer not null auto_increment,
+  `UserID` integer not null,
+  `Text` varchar(255),
+  `Source` varchar(255),
+  PRIMARY KEY (`PostID`),
+  FOREIGN KEY (`UserID`) REFERENCES REGISTERED_USER(`UserID`)
+);
+
+DROP TABLE IF EXISTS 'LIKES' ;
+CREATE TABLE 'LIKES'(
+  `UserID` integer not null,
+  `PostID` integer not null,
+  FOREIGN KEY (`UserID`) REFERENCES REGISTERED_USER(`UserID`),
+  FOREIGN KEY (`PostID`) REFERENCES POST(`PostID`)
+);
+
+DROP TABLE IF EXISTS 'COMMENTS' ;
+CREATE TABLE 'COMMENTS'(
+  `CommentID` integer not null auto_increment,
+  `PostID` integer not null,
+  `UserID` integer not null,
+  FOREIGN KEY (`PostID`) REFERENCES POST(`PostID`),
+  FOREIGN KEY (`UserID`) REFERENCES REGISTERED_USER(`UserID`),
+  PRIMARY KEY (`CommentID`)
+);
+
+/*DROP TABLE IF EXISTS 'COMMENT_DETAILS' ;
+CREATE TABLE 'COMMENT_DETAILS'(
+	`CommentID` integer not null,
+	`UserID` integer not null,
+	PRIMARY KEY (`CommentID`),
+	FOREIGN KEY (`UserID`) REFERENCES REGISTERED_USER(`UserID`)
+);*/
+
+INSERT INTO `REGISTERED_USER` (`Username`,`Name`,`Surname`,`Email`,`Password`, `UserType`) VALUES
+  (1, 'nick', 'Nicholas', 'Rader', 'nick@email.co.za', '$2y$10$20lIJidCeh.z.BGGupMMrOFPtSMmNLLaOOgO1xhr3SxEQsTYKKoGW',3),
+  (2, 'byzo', 'Byron', 'Mills', 'byron@email.co.za', '$2y$10$20lIJidCeh.z.BGGupMMrOFPtSMmNLLaOOgO1xhr3SxEQsTYKKoGW',1),
+  (3, 'jords', 'Jordan', 'Van Vuuren', 'jordan@email.co.za', '$2y$10$20lIJidCeh.z.BGGupMMrOFPtSMmNLLaOOgO1xhr3SxEQsTYKKoGW',1);
+
+INSERT INTO `FOLLOWERS` (`UserID`,`FollowerID`) VALUES
+  (2,3),
+  (3,2);
+
+INSERT INTO `FOLLOWING` (`UserID`,`FollowingID`) VALUES
+  (2,3),
+  (3,2),
+  (1,2),
+  (1,3);
+
